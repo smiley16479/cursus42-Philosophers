2cd2debe13c96141ba01b7df1711cb88ff849b38
smiley16479 <smiley16479@aol.com>
2021-03-14 15:19:05 +0100

    philo_two/three added

--- philo_one/main.c
+++ philo_one/main.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adtheus <adtheus@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/03/12 19:20:23 by adtheus           #+#    #+#             */
+/*   Updated: 2021/03/13 12:05:10 by adtheus          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 /*
 number_of_philosopher 
 time_to_die 
@@ -24,9 +36,77 @@ int main(int ac, char **av)
 	while (!is_phy_dead_or_satiated(&d, &start))
 		;
 	*(d.tab[0].time_to_stop) = 1;
-	// usleep(100);
-	// free(d.tab);
+	usleep(100000);
+	free(d.tab);
 	return 0;
-	// pthread_exit(NULL);
 }
 
+/*
+** Function basiques
+*/
+
+int ft_atoi(char *str)
+{
+	int i;
+	int nb;
+
+	i = -1;
+	nb = 0;
+	while (str[++i])
+	{
+		nb *= 10;
+		if (str[i] == '-' || !('0' <= str[i] && str[i] <= '9'))
+			return (-1);
+		nb += str[i] - '0';
+	}
+	return (nb == 0 ? -1 : nb);
+}
+
+/*
+** Function de setup
+*/
+
+int set_up_sub1(t_data *d, struct timeval *start, int *time_to_stop)
+{
+	int i;
+
+	i = -1;
+	while (++i < d->data[0])
+	{
+		d->tab[i].whoami = i;
+		d->tab[i].lunch_nb = 0;
+		d->tab[i].start = start;
+		d->tab[i].last_lunch = 0;
+		d->tab[i].data = &(d->data);
+		d->tab[i].time_to_stop = time_to_stop; // NULL;
+		d->tab[i].next = &(d->tab[(i + 1) % d->data[0]]);
+		if (pthread_mutex_init(&(d->tab[i].mutex), NULL))
+			return (1);
+	}
+	// printf("%d\n%d\n%f\n", d->tab[0].whoami, (*d->tab).lunch_nb, d->tab[0].last_lunch);
+	i = -1;
+	while (++i < d->data[0])
+	{
+		pthread_create(&(d->tab[i].th_nb), NULL, (void*)thread_phy, &(d->tab[i]));
+		// pthread_detach(d->tab[i].th_nb);
+	}
+	return (0);
+}
+
+int set_up(struct timeval *start, t_data *d, int *time_to_stop)
+{
+	d->data[e_m_eat] = -1;
+	if (!(d->ac == 5 || d->ac == 6))
+	{
+		write(1, "Wrong arg number\n", 17);
+		return (1);
+	}
+	while (d->ac-- > 1)
+		if ((d->data[d->ac - 1] = (ft_atoi(d->av[d->ac]) * ((d->ac >= 2 && d->ac <= 4) ? 1000 : 1))) == -1)
+			return (-1);
+	d->tab = malloc(d->data[0] * sizeof(t_phy));
+	if (gettimeofday(start, NULL))
+		return (-1);
+	set_up_sub1(d, start, time_to_stop);
+	return (0);
+}
--- philo_one/phisiology.c
+++ philo_one/phisiology.c
@@ -1,3 +1,14 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   phisiology.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adtheus <adtheus@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/03/12 19:20:34 by adtheus           #+#    #+#             */
+/*   Updated: 2021/03/12 19:20:37 by adtheus          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "philosophers.h"
 
@@ -27,9 +38,7 @@ int	is_phy_dead_or_satiated(t_data *d, struct timeval *start)
 	while (++i < d->data[0])
 		if ((now - d->tab[i].last_lunch) > d->data[1] / 1000)
 		{
-			printf("phy[%d] is dead : %f since last_lunch(%f) & now(%f)\n",
-			i , (now - d->tab[i].last_lunch), d->tab[i].last_lunch, now);
-			printf("%d\n",(*d->tab[0].time_to_stop = 1));
+			printf("%5.0f %d died\n", (now - d->tab[i].last_lunch), i);
 			return (((*d->tab[0].time_to_stop )= 1));
 		}
 	i = -1;
@@ -41,7 +50,7 @@ int	is_phy_dead_or_satiated(t_data *d, struct timeval *start)
 		if (i == d->data[0])
 		{
 			printf("all phy have eatten %d lunches\n", (*d->tab).lunch_nb);
-			return (2);
+			return (((*d->tab[0].time_to_stop )= 2));
 		}
 	}
 	return (0);
@@ -49,18 +58,17 @@ int	is_phy_dead_or_satiated(t_data *d, struct timeval *start)
 
 int print_status(char act, t_phy *p)
 {
-	// if (*p->time_to_stop)
-	// 	return (1);
+	if (*p->time_to_stop)
+		return (1);
 	if (act == 1)
-		printf("%5.0f %d has taken his forks (%p & %p)\n",
-		get_time(p->start), p->whoami + 1, &p->mutex, &p->next->mutex);
+		printf("%5.0f %d has taken a fork\n",
+		get_time(p->start), p->whoami + 1);
 	else if (act == 2)
-		printf("%5.0f %d is eating (for %d millisec)\n",
-		get_time(p->start), p->whoami + 1, (*p->data)[2]);
+		printf("%5.0f %d is eating\n", get_time(p->start), p->whoami + 1);
 	else if (act == 3)
-		printf("%5.0f %d is sleeping\n", get_time(p->start), p->whoami);
+		printf("%5.0f %d is sleeping\n", get_time(p->start), p->whoami + 1);
 	else if (act == 4)
-		printf("%5.0f %d is thinking\n", get_time(p->start), p->whoami);
+		printf("%5.0f %d is thinking\n", get_time(p->start), p->whoami + 1);
 	return (0);
 }
 
@@ -70,22 +78,32 @@ void* thread_phy (t_phy *p)
 	{
 		p->whoami % 2 ? PML(&p->mutex) : PML(&p->next->mutex);
 		// p->whoami % 2 ? printf("%5.0f %d has taken his right fork (mutext :%p)\n", get_time(p->start), p->whoami, &p->next->mutex) : printf("%5.0f %d has taken his left fork (mutext :%p)\n", get_time(p->start), p->whoami, &p->mutex);
+		if (print_status(1, p))
+			break ;
 		p->whoami % 2 ? PML(&p->next->mutex) : PML(&p->mutex);
 		// p->whoami % 2 ? printf("%5.0f %d has taken his left fork (mutext :%p)\n", get_time(p->start), p->whoami, &p->mutex) : printf("%5.0f %d has taken his right fork (mutext :%p)\n", get_time(p->start), p->whoami, &p->next->mutex);
-		printf("%5.0f %d has taken his forks (%p & %p)\n", get_time(p->start), p->whoami + 1, &p->mutex, &p->next->mutex);
-		printf("%5.0f %d is eating (for %d millisec)\n", get_time(p->start), p->whoami + 1, (*p->data)[2]);
+		if (print_status(1, p))
+			break ;
+		if (print_status(2, p))
+			break ;
+		// printf("%5.0f %d has taken his forks (%p & %p)\n", get_time(p->start), p->whoami + 1, &p->mutex, &p->next->mutex);
+		// printf("%5.0f %d is eating (for %d millisec)\n", get_time(p->start), p->whoami + 1, (*p->data)[2]);
 		p->lunch_nb++;
 		usleep((*p->data)[2]);
-		printf("lst_lunch de %d: %f\n",p->whoami + 1, (p->last_lunch = get_time(p->start)));
+		p->last_lunch = get_time(p->start); // printf("lst_lunch de %d: %f\n",p->whoami + 1, (p->last_lunch = get_time(p->start)));
 		p->whoami % 2 ? PMUL(&p->next->mutex) : PMUL(&p->mutex); // On déverrouille le mutex de sa fourchette
 		// p->whoami % 2 ? printf("%5.0f %d has realeased his right fork ptr p->next->mutex [%p]\n", get_time(p->start), p->whoami, &p->next->mutex) : printf("%5.0f %d has realeased his left fork ptr p->mutex [%p]\n", get_time(p->start), p->whoami, &p->mutex);
 		p->whoami % 2 ? PMUL(&p->mutex) : PMUL(&p->next->mutex); // On déverrouille le mutex de la fourchette du voisin
 		// p->whoami % 2 ? printf("%5.0f %d has realeased his left fork ptr p->mutex [%p]\n", get_time(p->start), p->whoami, &p->mutex) : printf("%5.0f %d has realeased his right fork ptr p->next->mutex [%p]\n", get_time(p->start), p->whoami, &p->next->mutex);
 		// printf("%5.0f %d has realeased the forks (%p & %p)\n", get_time(p->start), p->whoami, &p->mutex, &p->next->mutex);
-		printf("%5.0f %d has realeased the forks\n", get_time(p->start), p->whoami + 1);
+		// printf("%5.0f %d has realeased the forks\n", get_time(p->start), p->whoami + 1);
 		// printf("%5.0f %d is sleeping\n", get_time(p->start), p->whoami);
+		if (print_status(3, p))
+			break ;
 		usleep((*p->data)[3]);
 		// printf("%5.0f %d is thinking\n", get_time(p->start), p->whoami);
+		if (print_status(4, p))
+			break ;
 	}
 	return (NULL);
 }
